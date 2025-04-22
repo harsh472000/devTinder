fb743f2931ee68e37e47bdf070652aac3d807b80
harsh-gymble <harsh.gymble@gmail.com>
2025-04-22 22:34:16 +0530

    fix: database url chabnges

--- src/config/database.js
+++ src/config/database.js
@@ -4,7 +4,7 @@ const connectDB = async () => {
   // await mongoose.connect(
   //   "mongodb://localhost:27017/devTinder"
   // );
-  await mongoose.connect("mongodb://127.0.0.1:27017/devTinder");
+  await mongoose.connect("mongodb+srv://harshmeghani47:1G15QTaqyXlEzslw@namasteharsh.popfo95.mongodb.net/?retryWrites=true&w=majority&appName=NamasteHarsh");
 };
 
 module.exports = connectDB;
